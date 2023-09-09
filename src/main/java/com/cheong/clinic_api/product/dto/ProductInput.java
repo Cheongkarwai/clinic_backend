b6a72cf0700365c404c0d79d22732b3c066467a3
Cheong Kar Wai <cheongkarwai5@gmail.com>
2023-09-09 12:00:45 +0800

    adding new properties (description,price)

--- src/main/java/com/cheong/clinic_api/product/dto/ProductInput.java
+++ src/main/java/com/cheong/clinic_api/product/dto/ProductInput.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +14,8 @@ import lombok.NoArgsConstructor;
 public class ProductInput {
 
 	private String name;
+
+	private String description;
+
+	private BigDecimal price;
 }
