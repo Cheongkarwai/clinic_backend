d549d46d950c82a08ca52080f20114ecff224c98
Cheong Kar Wai <cheongkarwai5@gmail.com>
2023-09-09 12:03:11 +0800

    update package

--- src/main/java/com/cheong/clinic_api/user/domain/UserProfile.java
+++ src/main/java/com/cheong/clinic_api/user/domain/UserProfile.java
@@ -1,13 +1,6 @@
 package com.cheong.clinic_api.user.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import org.springframework.dao.DataAccessException;
 
